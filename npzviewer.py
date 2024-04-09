d3b889abd2f824ab0683436c1999eb513f2b999c
Josh Eyres <josh.m.eyres@gmail.com>
2024-04-09 04:26:58 +0100

    Initial project commit

---
+++ npzviewer.py
@@ -0,0 +1,19 @@
+import numpy
+import matplotlib.pyplot as plt
+import torch
+import numpy
+from tqdm.auto import tqdm
+
+from point_e.diffusion.configs import DIFFUSION_CONFIGS, diffusion_from_config
+from point_e.diffusion.sampler import PointCloudSampler
+from point_e.models.download import load_checkpoint
+from point_e.models.configs import MODEL_CONFIGS, model_from_config
+from point_e.util.plotting import plot_point_cloud
+from point_e.util.point_cloud import PointCloud
+
+pc = PointCloud.load("Pointclouds/a house")
+pc.coords[0] = (-1,-1,-1)
+print(pc.coords[0])
+fig = plot_point_cloud(pc, grid_size=1, fixed_bounds=((-1, -1, -1), (1, 1, 1)))
+
+plt.show()
